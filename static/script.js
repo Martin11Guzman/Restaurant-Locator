6b656c9b25d3477e363cc9b371a3bf3a3c3256ed
Unknown <mguzman@basecampcodingacademy.org>
2016-12-22 11:43:24 -0600

    implemented an iframe video, added css for the table

--- static/script.js
+++ static/script.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
 	/* This code is executed after the DOM has been completely loaded */
-	
+
 	$('nav a,footer a.up').click(function(e){
-										  
+
 		// If a link has been clicked, scroll the page to the link's hash target:
-		
+
 		$.scrollTo( this.hash || 0, 1500);
 		e.preventDefault();
 	});
 
+});
+$(document).ready(function() {
+  $('#play-video').on('click', function(ev) {
+
+    $("#video")[0].src += "&autoplay=1";
+    ev.preventDefault();
+
+  });
 });
